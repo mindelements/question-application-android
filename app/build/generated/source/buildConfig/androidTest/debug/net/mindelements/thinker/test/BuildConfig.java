fed29e059cfa1fed12e7e7958627ceb7fe946872
Glass Water <Glass Water>
2016-04-13 01:16:52 -0500

    added app-release.apk.bin

--- app/build/generated/source/buildConfig/androidTest/debug/net/mindelements/thinker/test/BuildConfig.java
+++ app/build/generated/source/buildConfig/androidTest/debug/net/mindelements/thinker/test/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "net.mindelements.thinker.test";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 72420151;
+  public static final int VERSION_CODE = 72420152;
   public static final String VERSION_NAME = "1.0";
 }
